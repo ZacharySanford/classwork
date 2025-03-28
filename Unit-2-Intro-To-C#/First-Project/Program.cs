50a17768d5c7616937761d5c6ae5674266b640c9
Zach Sanford <afavorhouse2113@gmail.com>
2025-03-27 21:57:49 -0400

    First Commit

---
+++ Unit-2-Intro-To-C#/First-Project/Program.cs
@@ -0,0 +1,60 @@
+﻿// This is a comment - everything after the "/" is ignored
+/*
+ *This is a block comment - everything between the slash-asterisk is a comment 
+ * 
+ *This is useful if you have a lot to say or to deactivate code
+ * 
+ * 
+ * The * at the start of each line is optional
+ */
+
+
+// A namespace identifies a context in which a word is known
+// allows the same name to be used in different context to mean different things
+namespace First_Project
+{
+
+    // Everything in C# is defined in a class
+    // a class is a group of related things
+    // a class starts with the class keyword
+    class Program
+
+        //{} surround blocks of related code aka code-blocks
+
+    { // Start of code for class
+        // Every application has a method called Main()
+        // Method is a self-contained set of code that performs a function
+        // Also known as a function or simply a program
+        // A method may return a value and accept data to process
+        // Method Signature identifies what the method returns, its name, and what it accepts
+        //      return-type name(data-it-accepts)
+        // Main() is where every application starts
+        // Every application has one and only one Main() method
+
+        // Below Main() is a method that returns void and accepts string[] called args
+
+        // void means it returns nothing
+        // static means there can only be one of these in the application
+
+        //  return
+        //  type name(data-it-accepts)
+        static void Main(string[] args)
+        { // Start of the code for Main ()
+            // This code will display whatever is between the ("") on the screen
+
+            // Console is an object defined by C# to represent the screen
+            // An object has certain behaviors
+            // A behavior is something an object can do with data
+            // Behaviors are implemented using methods
+
+            // object.method(data) - Object Oriented Programming
+
+            // When you see a . after a name, the name to the left of the dot is probably an object. 
+            // name( <-- the name is a method
+            // name after the . the name is usually a method
+            // object.Method(data-for-the-method-to-process)
+
+            Console.WriteLine("Hello, World!"); // EVERY C# STATEMENT ENDS WITH A ;
+        } // End of the code for Main()
+    } // End of code for the class
+} 
---
+++ Unit-2-Intro-To-C#/FirstProject/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace FirstProject
+{
+
+    //This program will ask for three numbers
+    //      add them together and display the total
+
+    // Programming solution - Human thinking
+    // Identify the detailed steps
+
+    // Ask for the numbers one at a time
+    // Write down each number as it's given
+    // Add the numbers together to create a sum/total
+    // Tell the requester the total (display)
+
+    // Now that we have the steps in our solution
+    // Identify any data/processes you need for the steps
+
+    // 1. A place to write down each number - a variable in a program
+    // 2. A place to hold the sum/total - a variable in a program/expression to add
+    // 3. A way to ask for the numbers - a method in a program (object.method())
+    // 4. A way to receive the numbers - a method in a program (object.method())
+    // 5. A way to report the sum/total - a method in a program (object.method())
+
+    // Place the steps in order and provide more detail if necessary if needed to solve the problem
+    // 1. Ask for the numbers one at a time
+    //      a. Tell whatever is giving us the numbers to give me the first number
+    //      b. Receive the first number from sender
+    //      c. Write down the number as it's given
+    //      Repeat  two more times
+    // 2. Add the numbers together to create a sum/total
+    //      a. Add the first number
+    //      b. Add the second number
+    //      c. Add the third number
+    //      d. Store the number
+    // 3. Share the sum
+    
+    /*
+     * To define a variable: data-type name = initial value;
+     * 
+     * 
+     * Commonly used data types in C#:
+     * 
+     *  int     - Numeric data that is a whole nuimber between +/- 2 billion
+     *  long    - A whole number greater than +/- 2 billion
+     *  double  - Numberic value with decimal places
+     *  char    - a single alpha-numeric character
+     *  string  - a series of alpha-numeric characters (words or sentence)
+     *  bool    - true or false value for conditions
+     *  
+     *  
+     *  examples:   10 - int (or long)
+     *              1.0 - double
+     *              'a' - char 
+     *              "some words" - string 
+     *              
+     *  variable names in C# are usually spelled in camelCase
+     *  PascalCase 
+     * 
+     * **************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Starting Program ---");
+
+            Console.Write("Please enter a number: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a second number:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a third number: ");
+            int number3 = int.Parse(Console.ReadLine());
+            
+
+            int sum = number1 + number2 + number3;
+           
+            Console.WriteLine($"{number1} + {number2} + {number3} = {sum}");
+
+            Console.WriteLine("--- Ending Program ---");
+        }
+    }
+}
