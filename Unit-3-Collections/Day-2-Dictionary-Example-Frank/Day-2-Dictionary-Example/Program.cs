73db096c47e9e9397880b5ed22726f65a0bc62a3
Zach Sanford <afavorhouse2113@gmail.com>
2025-04-21 18:17:58 -0400

    commit 042125

--- Unit-3-Collections/Day-2-Dictionary-Example-Frank/Day-2-Dictionary-Example/Program.cs
+++ Unit-3-Collections/Day-2-Dictionary-Example-Frank/Day-2-Dictionary-Example/Program.cs
@@ -1,126 +1,101 @@
-﻿using System;
-using System.Collections.Generic;  // Give me access to the systems generic collections stuff
-
-namespace Day_2_Dictionary_Example
+﻿namespace Day_2_Dictionary_Example
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*********************************************************
-             * Example use of a Dictionary
-             *********************************************************/
+        //    /*********************************************************
+        //     * Example use of a Dictionary with array/List value
+        //     *********************************************************/
 
-            // Create a Dictionary for relate zip codes people live in
+
+            Dictionary<string,List <double>> gradeBook = new();
+
+            // Lets add students and grades until the user indicates they are done
+            // Loop until the user satisfies a condition instead of a specific # of times
+            // Replace the for-loop with either a while-loop or do-while()
+            // Do we want to loop at least once? Yes - do-while loop; No - while loop
+            // Is it ok to loop 0 times? Yes - while loop; No - do-while loop
             //
-            //  Key - Persons Name - Unique Id for an entry
-            //  (string)
-            //
-            //  value - Zip Code  - data associated with Key
-            //  (int)
-            //
-            // Syntax:   Dictionary<key-type, value-type> = new Dictionary<key-type, value-type)
+            // In this case we have to ask the user at least once to enter some data
+            // How do we want the user to let us know they are done?
+            // Create an on/off switch
+            // Prompt - ask if they are done
 
-         // data-type<key-type, value-type> name = new data-type<key-type, value>();  
-            Dictionary<string, int> personInfo   = new Dictionary<string, int>();
-
-            // Add some people and their zip codes
-            //
-            // dictionaryName[key] = value;
-            //
-            // dictionaryName.Add(key, value) 
-
-            personInfo["Frank"]  = 85339;
-            personInfo["Kirk"]   = 48009;
-            personInfo["Spock"]  = 15658;
-            personInfo["Bones"]  = 49116;
-
-            personInfo.Add("Jenna", 85339);
-
-            // Specifying a key that already exists in the Dictionary
-            // When using [], the entry for the key is update
-            personInfo["Frank"] = 44143;
-        //  personInfo.Add("Frank", 12345);  // using .Add with an existing key throws an exception
-        
-            personInfo.Remove("Frank");  // Remove the entry with this key
-        //  Console.WriteLine("Frank lives in: " + personInfo["Frank"]);  // throws Exception
-
-            // Specifying a key that already exists in the Dictionary
-            // when using .Add() an exception is thrown if key already exists
-            // So to avoid this and still use .Add() check to see if the key already
-            //       exists in Dictionary and only do teh .Add if it is not
-            // if the key value is NOT in the dictionary...
-            if (!personInfo.ContainsKey("Frank"))
-            {
-                personInfo.Add("Frank", 06082);  // Add it
-            }
-            else // If key IS already in the Dictionary
-            {
-                personInfo["Frank"] = 06082;    // Update its value
-            }
-
-            Console.WriteLine("Kirk lives in: " + personInfo["Kirk"]);
-
-            Console.Write("Whose zip code do you want? ");
-            string name = Console.ReadLine();
-
-            // Handle Exception if the key not beinging in the Dictionary
-            try
-            {
-                Console.WriteLine(name + " lives in: " + personInfo[name]);
-            }
-            catch (KeyNotFoundException exceptionInfo)
-            {
-                Console.WriteLine("Error looking for: " + name);
-                Console.WriteLine(exceptionInfo.Message);
-            }
-
-            // If you want to walk through the entire Dictionary with a foreach
-            //    You need to get all the keys in the Dictionary
-            //
-            // use the KeyValuePair object in the foreach
-            //
-            // KeyValuePair<key-type, value-type> name-for-dictionary-entry
-            //
-            // KeyValuePair return the key and associated value from the Dictionary
-            //
-            // .Key - access the key
-            // .Value - access the value
-
-            // get each key/value pair and store it in variable called anEntry
-            foreach (KeyValuePair<string, int> anEntry in personInfo)
-            {
-              //Console.WriteLine("Youssef wanted this: " + anEntry);
-                Console.WriteLine(anEntry.Key + " lives in zip code " + anEntry.Value);
-            }
-                 
-            // Ask the user for a name and a grade
-            // Store them in a Dictionary
-            
-            // Dictionary to hold name (key) and grade (value)
-            //         key     value
-            //         type    type    name      = new Dictionary<key-type, value>();
-            Dictionary<string, double> gradeBook = new Dictionary<string, double>();
-            
-            // Lets add 3 students - loop 3 times for-loop
-            for (int i = 0; i < 3; i++)
+            // Set a condition based on what they tell us
+            string userResponse = "";
+            double grade = 0;
+            string userGrade = "";
+            List<double> grades = new();
+            do
             {
                 // Ask the user for the student name and grade
                 Console.Write("Enter student name: ");
                 string studentName = Console.ReadLine(); // Get student name// Ask the user for the student name
-                Console.Write("Enter grade: ");
-                double grade = Double.Parse(Console.ReadLine()); // Get student grade
+                //Console.Write("Enter grade: ");
+                // Because the user might enter a non-numeric grade causing an exception
+                // We should handle the exception so the program doesn't end
+                //   with a message that will scare the human
+                // We can use a try/catch to handle the exception
+                // When the user enters a non-numeric grade:
+                //      1. ignore it - if we do this
+                //                     don't put student data in dictionary
+                //      2. Give them another chance
+                //      3. Set the value to 0
+                //
+                try // A statement inside this try block might cause an exception
+                { // We need to get multiple grades for each student and store them in a List
 
-                // Add the data to our Dictionary
+                    string whatTheyTyped = "";
+                    
+                    while (whatTheyTyped != "end") 
+                    {
+                        Console.Write("Enter grade or end: ");
+                    //Get the user input as a string in case we need it later
+                    whatTheyTyped  = Console.ReadLine();
+                    if (whatTheyTyped == "end")
+                        {
+                            break; // exit the loop - continue would be OK too
+                        }
+                    grade = Double.Parse(userGrade); // convert the user input to number
+                    grades.Add(grade); // Add the grade entered to the List
+                    }
+                    // at the end of this look the grades list has all the grades
+                }
+                catch (FormatException exceptionObject) // if it throws a FormatException ...
+                {
+                    Console.WriteLine($"The data you entered ({userGrade}) is not a valid number");
+                    Console.WriteLine("The data is ignored");
+                    // Skip adding the student to the dictionary
+                    continue; // skip the rest of the loop processing
+                }
+                
+
+                // Add the data to our Dictionary only if we have a valid grade
                 // Dictionary[key}     = value;
-                gradeBook[studentName] = grade; // gradeBook.Add(studentName, grade) // may cause an exception
-            }
+                gradeBook[studentName] = grades; // adding list of grades for the student
+                userResponse = "";
+                while (userResponse != "y" && userResponse != "n") { 
+
+                    Console.WriteLine("Are you done?");
+                    userResponse = Console.ReadLine().ToLower(); 
+                    if(userResponse != "y" && userResponse != "n")
+                    {
+
+                        Console.WriteLine("Sorry we only accept 'y' or 'n'");
+                    }
+
+                }
+               
+
+            } while (userResponse == "n");
             
             // Display the entrys in our Dictionary
             // Use a KeyValuePair type to get an entry from teh Dictionary
-            foreach (KeyValuePair<string, double> anEntry in gradeBook)
+            foreach (KeyValuePair<string, List <double>> anEntry in gradeBook)
             {
-                Console.WriteLine(gradeBook[anEntry.Key] + " has a grade of " + anEntry.Value);
+                Console.WriteLine(anEntry.Key + " has a grade of " + anEntry.Value);
+                foreach (double grade in grades)
             }
             
             Console.WriteLine("Please press enter to end program...");
