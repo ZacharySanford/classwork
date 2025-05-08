51f871cd495c4a90d469fa72b173184f8c056d77
Zach Sanford <afavorhouse2113@gmail.com>
2025-05-08 18:19:59 -0400

    commit one 05082025

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/MovieLab-20250507T235609Z-001/MovieLab/Movie.cs
@@ -0,0 +1,65 @@
+﻿
+    public class Movie
+    {
+    private string title;
+    private string category;
+
+
+    public string Title
+    {
+        get {return title;}
+        set {title = value;}
+    }
+    
+    public string Category
+    {
+        get {return category;}
+        set {category = value;}
+    }
+    public Movie(string movieTitle, string movieCategory)
+        {
+            title = movieTitle;
+            category = movieCategory;
+
+        }
+
+    public Movie(Movie sourceMovie)
+    {
+        title = sourceMovie.Title;
+        category = sourceMovie.Category;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title} Category: {Category}";
+    }
+
+    public override bool Equals(object otherObject)
+    {
+        if (otherObject.GetType() != this.GetType()) 
+        {
+            return false;
+        }
+
+        if (otherObject == this)
+        {
+            return true;
+        }
+
+        Movie otherMovie = (Movie)otherObject;
+
+        if(otherMovie.title == this.title || otherMovie.category == this.category)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+            
+    }
+
+
+}
+
+
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/MovieLab-20250507T235609Z-001/MovieLab/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MovieLab
+{
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting program");
+            string userCategory = "";
+            string userInput = "";
+            bool exit = false;
+            bool userContinue = true;
+
+            List<Movie> movies = new List<Movie>();
+
+            Movie myNeighborTotoro = new Movie("My Neighbor Totoro", "animated");
+            movies.Add(myNeighborTotoro);
+            Movie shawshankRedemption = new Movie("The Shawshank Redemption", "drama");
+            movies.Add(shawshankRedemption);
+            Movie schindlersList = new Movie("Schindler's List", "drama");
+            movies.Add(schindlersList);
+            Movie insidious = new Movie("Insidious", "horroe");
+            movies.Add(insidious);
+            Movie theConjuring = new Movie("The Conjuring", "horror");
+            movies.Add(theConjuring);
+            Movie aNewHope = new Movie("Star Wars Episode IV: A New Hope", "scifi");
+            movies.Add(aNewHope);
+            Movie alienPredator = new Movie("Alien vs Predator", "scifi");
+            movies.Add(alienPredator);
+            Movie pokemonTheMovie = new Movie("Pokemon The Movie", "animated");
+            movies.Add(pokemonTheMovie);
+            Movie yourName = new Movie("Your Name", "animated");
+            movies.Add(yourName);
+            Movie rememberTheTitans = new Movie("Remember The Titans", "drama");
+            movies.Add(rememberTheTitans);
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Would you like to look at Animated, Drama, Horror, or SciFi?");
+                    userCategory = Console.ReadLine().ToLower().Trim();
+
+                    if (userCategory != "animated" && userCategory != "drama" && userCategory != "horror" && userCategory != "scifi")
+                    {
+                        Console.WriteLine("That input was incorrect. Please enter Animated, Drama, Horror, or SciFi.");
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                } while (userCategory != "animated" && userCategory != "drama" && userCategory != "horror" && userCategory != "scifi");
+
+                foreach (Movie movie in movies)
+                {
+                    if (movie.Category.Equals(userCategory))
+                    {
+                        Console.WriteLine(movie);
+                    }
+                }
+
+                do
+                {
+                    Console.WriteLine("Would you like to search again? Y/N");
+                    userInput = Console.ReadLine().ToLower().Trim();
+                    if(userInput != "y" && userInput != "n")
+                    {
+                        Console.WriteLine("That input is invalid. Please enter Y or N.");
+                        
+                    }
+                    else
+                    {
+                        if(userInput == "n")
+                        {
+                            userContinue = false;
+                            exit = true;
+                            break;
+                        }
+                        else if(userInput == "y")
+                        {
+                            continue;
+                        }
+                    }
+                } while (userContinue);
+            } while (!exit);
+
+            Console.WriteLine("Ending Program");
+        }
+    }
+}
