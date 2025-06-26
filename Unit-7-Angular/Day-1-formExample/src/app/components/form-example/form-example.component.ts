a40fcde5c3eaa4f03863637c0aacf437389b8264
Zach Sanford <afavorhouse2113@gmail.com>
2025-06-26 18:38:46 -0400

    first push 062625

--- Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
@@ -11,9 +11,25 @@ import { throwError } from 'rxjs';
 })
 // Allow access to stuff defined this component from outside the component
 export class FormExampleComponent {
+  // Data to be shared with Angular when processing the web page
   buttonClicked = false
+  firstName = ""
+  lastName = ""
+  birthDate = ""
+  foodPref = ""
+  emailAddress = ""
+  emailPref = ""
+  prefSports : any[]
+  // Methods to handle interactions with the html/webpage
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
     console.log(`OK.... You Clicked Submit`)
+    console.log(`You entered ${this.firstName} in the first name field`)
+    console.log(`You entered ${this.lastName} in the last name field` )
+    console.log(`You entered ${this.birthDate} in the birthday field`)
+    console.log(`You entered ${this.foodPref} in the food preference field`)
+    console.log(`You entered ${this.emailAddress} in the food preference field`)
+    console.log(`You entered ${this.emailPref} in the food preference field`)
+    console.log(`You entered ${this.prefSports} in the sports preferences field`)
   }
 }
