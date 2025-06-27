9996fdb6b6b427e2fff7279db327ed34aa7a7999
Zach Sanford <afavorhouse2113@gmail.com>
2025-06-26 21:55:46 -0400

    final push 062625

--- Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { throwError } from 'rxjs';
@@ -5,7 +6,7 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'form-example',  // Name used in html to reference the component <form-example>
   standalone: true,
-  imports: [FormsModule],  // Tell Angular we want Forms support for this code
+  imports: [FormsModule, CommonModule],  // Tell Angular we want Forms support for this code
   templateUrl: './form-example.component.html',
   styleUrl: './form-example.component.css'
 })
@@ -15,7 +16,7 @@ export class FormExampleComponent {
   buttonClicked = false
   firstName = ""     // hold the data from first name from the form
   lastName = ""      // hold the data from last name from the form
-  birthday = ""      // hold the data from birthday from the form
+  birthday = ""     // hold the data from birthday from the form
   foodPreference = ""
   emailAddress = ""
   sendEmail = ""
@@ -24,8 +25,17 @@ export class FormExampleComponent {
   // With each element in the array corresponding to a choice
   // Checkbox need to be included in the html a individual element
   // More tomorrow
-  //sports : any[] 
-  sports = ""
+  sports : any[] = [
+    {name: 'European Football', value:"Soccer"},
+    {name: 'American Football', value:"Football"},
+    {name: 'Baseball', value:"Baseball"},
+    {name: 'Basketball', value:"Basketball"},
+    {name: 'Cricket', value:"Cricket"},
+    {name: 'Hockey', value:"Hockey"},
+    {name: 'Other', value:"Other Sport"},
+    {name: 'None', value:"None"},
+  ]
+  //sports = ""
  // Methods to handle interactions with the html/web page
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
@@ -33,6 +43,32 @@ export class FormExampleComponent {
     console.log(`You entered ${this.firstName} in the First Name field`)
     console.log(`You entered ${this.lastName} in the Last Name field`)
     console.log(`You entered ${this.emailAddress} in the email field`)
-    console.log(`You entered ${this.sports} as sports you like`)
+    //console.table(this.sports)
+
+    // console.table(this.checkboxResult) // This will call a function named checkboxResult
+    console.log("You indicated you like the following sports")
+    // Display each item checked on a line by itself
+    // Loop through an array of items that were checked and display them
+    // .forEach() - an array function that will loop through an array passing each element to an arrow function (=>)
+    // Use checkbox result function to get an array of checked items
+    // then give the array to .forEach to display each item
+    this.checkboxResult.forEach((aBox)=> console.log(aBox.name))
   }
+
+  get checkboxResult(){
+    //Loop through the checkbox array(sports)
+    //if an item has checked equals true, save for return when done
+    // When the loop is done, return all the items where checked was true
+    //
+    // .filet() is an array function that will go through an array one element at a time
+    //          using an arrow function (=>) to tell it whether the current element
+    //          should be saved in a new array
+    //
+    //          if the arrow (=>) returns true, the current element is saved in a new array
+    //
+    //          After all elements in the array are processed, it returns the array it saved elements in
+    return this.sports.filter((aCheckbox) => aCheckbox.checked)
+
+  }
+
 }
