d5a7eb2e8ebafb1afa7be20f4f6dcf3f7b8f6cd9
Zach Sanford <afavorhouse2113@gmail.com>
2025-07-30 18:42:10 -0400

    fp 073025

--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
@@ -1,12 +1,20 @@
 package com.firstapi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class for the Spring Boot application.
+// It contains the main method which is the entry point of the application
+// The @SpringBootApplication annotation indicates that this is a Spring Boot Application
+// This is what starts when you run the application
+
 @SpringBootApplication
 public class FirstapiApplication {
-
+		// The main method is the entry point of the application
 	public static void main(String[] args) {
+		// SpringApplication.run() method is used to launch the application.
+		// It takes the application class and command line arguments as parameters
 		SpringApplication.run(FirstapiApplication.class, args);
 	}
 
--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
@@ -1,13 +1,31 @@
 package com.firstapi;
 
+// This file contains controllercode to handle HTTP requests sent to this server
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// The @RestController annotation indicates that this class has REST controllers in it
+// REST stands for Representational State Transfer
+//  which is an architectural style for designing networked applications.
+// REST is a type of API - Most popular type of API in the world.
 
 @RestController
 public class myController {
 
-    @GetMapping
-    public String anyName() {
+    // The @GetMapping annotation maps HTTP GET requests to the specified method
+    @GetMapping()
+    // The following method will handle GET requests to the path http://localhost:8081/.
+    //      localhost is the name for any process running on your computer
+    //      8081 is the port number that the application is running on
+    public String anyName() { // The method name can be anything you want
+        // All this control does is return a string that says "Hi There"
         return "Hi There";
     }
+
+    @GetMapping("/welcome")
+        public String controllerMethodNamesDoNotEverMatter(){
+            return "Welcome to my first API";
+        }
+
 }
