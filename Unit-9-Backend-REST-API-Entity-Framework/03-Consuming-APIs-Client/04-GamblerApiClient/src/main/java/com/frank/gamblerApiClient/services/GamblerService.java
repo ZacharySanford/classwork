bfcc965d9d148a08caadb4ab5a79d1d54309b3ac
Zach Sanford <afavorhouse2113@gmail.com>
2025-08-06 18:20:50 -0400

    1st

--- Unit-9-Backend-REST-API-Entity-Framework/03-Consuming-APIs-Client/04-GamblerApiClient/src/main/java/com/frank/gamblerApiClient/services/GamblerService.java
+++ Unit-9-Backend-REST-API-Entity-Framework/03-Consuming-APIs-Client/04-GamblerApiClient/src/main/java/com/frank/gamblerApiClient/services/GamblerService.java
@@ -11,23 +11,50 @@ import com.frank.gamblerApiClient.models.*;
 
 public class GamblerService {
 
-  private final String BASE_URL;
+  // Usually the starting part of the URL for API is stored as a constant
+  // Other parts of the URL are added as needed
+  private final String BASE_URL; //Reference to the starting part of all URLs (usually server identifiers)
   private final RestTemplate restTemplate = new RestTemplate();
+    // RestTemplate is a Spring Framework class for performing REST API calls.
+    //
+    // RestTemplate contains methods for making HTTP requests
+    // and collect all exceptions that might happen into only two
+
+    // Define a reference to the RestTemplate
 
   public GamblerService() {
     BASE_URL = "http://localhost:8081/";
   }
+  // Assign the API URL base string to the BASE_URL
+
 
   /**
    * Retrieve all Gamblers in the data source
    * @return List<Gambler>
    */
   public Gambler[] getAllGamblers() {
+      // Use the getForObject RestTemplate method to execute a GET HTTP request to our API server
+      //
+      // It's called get ForObject() because it returns an Object not JSON
+      // You don't have to convert the JSON that comes back into an object.
+      //
+      // RestTemplate methods return either individual objects, array, objects, or nothing.
+      //
+      // RestTemplate may be used in any programming language
+      // Collection class structures like List, ArrayList, Map, Dictionary, Stack, Queue, Set et al.
+      // may be implemented differently in each programming language
+      //
+      // arrays are the same in all programming languages
+      //
+      // RestTemplate doesn't want to know how to create a Collection object of every programming language
+      // so it returns the most common data structure for all programming languages (array)
       Gambler[] theGamblers = null;
       try {
+          //                       getForObject(URL-for-the-API-call, What t
         theGamblers = restTemplate.getForObject(BASE_URL + "gamblers", Gambler[].class);
       } catch (RestClientResponseException expceptionObj) {
         // handles exceptions thrown by rest template and contains status codes
+        // Display the status code and any message related to the error
         System.out.println(expceptionObj.getRawStatusCode() + " : " + expceptionObj.getStatusText());
       } catch (ResourceAccessException exceptionObj) {
         // i/o error, ex: the server isn't running
